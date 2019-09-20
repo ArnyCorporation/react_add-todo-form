89eada7d5b1f432c88870b0503e1ebdc67b7483b
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-20 10:23:34 +0300

    Updates

---
+++ precache-manifest.aee56c0d9a9f92ea477a7a527076a1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2d0480824ab8325160258744dc5332",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "6ee813f58be94a964d2a",
+    "url": "/react_add-todo-form/static/css/main.1e50a6fd.chunk.css"
+  },
+  {
+    "revision": "6c8f1d725de506c0f583",
+    "url": "/react_add-todo-form/static/js/2.e8aa7888.chunk.js"
+  },
+  {
+    "revision": "6ee813f58be94a964d2a",
+    "url": "/react_add-todo-form/static/js/main.10851bc1.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
